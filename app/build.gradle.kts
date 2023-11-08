18bc6a619598a83da10676d57ac74bca8f3581f2
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-11-08 23:48:39 +0530

    completion of welcome page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,13 @@ android {
     namespace = "com.example.workshala2"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
+    buildFeatures {
+        dataBinding=true
+    }
     defaultConfig {
         applicationId = "com.example.workshala2"
         minSdk = 24
@@ -41,7 +48,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.intuit.ssp:ssp-android:1.1.0")
+    implementation ("com.intuit.sdp:sdp-android:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.android.material:material:1.0.0")
 }
--- app/src/main/java/com/example/workshala2/MainActivity.kt
+++ app/src/main/java/com/example/workshala2/MainActivity.kt
@@ -4,17 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.workshala2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-//        val login =findViewById<TextView>(R.id.textView2)
-//        login.setOnClickListener {
-//            val intent = Intent(this, SignupActivity::class.java)
-//            startActivity(intent)
-//        }
+        binding.next.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

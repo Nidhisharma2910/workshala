e8986e8a724f2bfb6b026c185bcacb48669a5e29
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-11-13 22:57:43 +0530

    adding email verification

--- app/src/main/java/com/example/workshala2/SignupActivity.kt
+++ app/src/main/java/com/example/workshala2/SignupActivity.kt
@@ -6,16 +6,20 @@ import android.os.Bundle
 import android.widget.TextView
 import android.text.InputFilter
 import android.text.InputFilter.LengthFilter
+import android.util.Patterns
 import android.widget.EditText
 import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.workshala2.databinding.ActivityMainBinding
 import com.example.workshala2.databinding.ActivitySignupBinding
+import com.google.android.material.textfield.TextInputLayout
 
 class SignupActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivitySignupBinding
 
     lateinit var editText: EditText
+    lateinit var editText1: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
@@ -26,13 +30,42 @@ class SignupActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.inputEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()) {
+                    binding.button.isEnabled = true
+                    binding.inputEmail.error = null // Clear any previous error
+                } else {
+                    binding.button.isEnabled = false
+                    binding.inputEmail.error = "Invalid Email"
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
 
         title = "Workshala2"
         editText = findViewById(R.id.first)
         val maxTextLength = 20
         editText.filters = arrayOf<InputFilter>(LengthFilter(maxTextLength))
 
+        title = "Workshala2"
+        editText1 = findViewById(R.id.last)
+        val maxTextLength1 = 20
+        editText1.filters = arrayOf<InputFilter>(LengthFilter(maxTextLength1))
+
     }
-}
+
+
+    }
+
+
+
 
 

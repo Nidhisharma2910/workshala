99e3e1cd75d9620d4e5409141ea08347971fa3d3
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-11-14 22:01:52 +0530

    completion of filter pages

--- app/src/main/java/com/example/workshala2/FilterEightActivity.kt
+++ app/src/main/java/com/example/workshala2/FilterEightActivity.kt
@@ -3,15 +3,14 @@ package com.example.workshala2
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.workshala2.databinding.ActivityFilterBinding
+import com.example.workshala2.databinding.ActivityFiltereightBinding
 
-class FilterActivity : AppCompatActivity() {
+class FilterEightActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityFilterBinding
+    private lateinit var binding: ActivityFiltereightBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFilterBinding.inflate(layoutInflater)
+        binding = ActivityFiltereightBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
--- app/src/main/java/com/example/workshala2/SignInActivity.kt
+++ app/src/main/java/com/example/workshala2/SignInActivity.kt
@@ -8,6 +8,7 @@ import android.text.TextWatcher
 import android.util.Log
 import android.util.Patterns
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.workshala2.databinding.ActivitySigninBinding
@@ -16,15 +17,30 @@ import java.util.logging.Logger
 import java.util.regex.Pattern
 
 class SignInActivity : AppCompatActivity() {
-
-
     private lateinit var binding: ActivitySigninBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val password = findViewById<EditText>(R.id.password)
+
+        password.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do something before the text changes
+            }
+
+            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                val pass = password.text.toString()
+                validatePassword(pass)
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                // Do something after the text has changed
+            }
+        })
+
+
 
         binding.forgot.setOnClickListener {
             val intent = Intent(this, ForgotActivity::class.java)
@@ -43,12 +59,15 @@ class SignInActivity : AppCompatActivity() {
                     binding.textInputEmail.error = "Invalid Email"
                 }
             }
-
             override fun afterTextChanged(s: Editable?) {
             }
         })
-
-
     }
 
+    fun validatePassword(password: String) {
+        val upperCase = Pattern.compile("[A-Z]")
+        val lowerCase = Pattern.compile("[a-z]")
+        val digitCase = Pattern.compile("[0-9]")
+
+    }
 }

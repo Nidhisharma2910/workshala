d94e55596282fbcbb593b23c803e809dccf40e70
params19 <paramsrivastava5@gmail.com>
2023-11-17 13:51:48 -0800

    Changes category page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -53,9 +53,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.google.android.material:material:1.0.0")
+    implementation ("com.google.android.material:material:1.10.0")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.7.1")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation  ("com.squareup.retrofit2:converter-gson:2.1.0")
     implementation ("com.google.code.gson:gson:2.10.1")
 //    implementation("com.squareup.okhttp3:okkttp:4.11.0")

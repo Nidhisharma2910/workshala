ca8379db1cd4226eb1f0be4a39160b5952a87dcd
params19 <paramsrivastava5@gmail.com>
2023-11-18 11:49:47 -0800

    home page edited and added responsiveness

--- app/src/main/java/com/example/workshala2/HomeActivity.kt
+++ app/src/main/java/com/example/workshala2/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.workshala2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,8 +11,14 @@ class HomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val aboutDoc=findViewById<TextView>(R.id.textView38)
+        aboutDoc.setOnClickListener{
+            val intent= Intent(this,AndroidLargeActivity::class.java)
+            startActivity(intent)
+    }
+
     }
 }
-
 
 

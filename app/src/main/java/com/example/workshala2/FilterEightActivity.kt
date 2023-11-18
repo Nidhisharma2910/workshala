dbc1956ddc8ced9855313759411f2258762e9d71
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-11-18 18:11:46 +0530

    changes in filter page and adding register api

--- app/src/main/java/com/example/workshala2/FilterEightActivity.kt
+++ app/src/main/java/com/example/workshala2/FilterEightActivity.kt
@@ -2,15 +2,80 @@ package com.example.workshala2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import com.example.workshala2.databinding.ActivityFiltereightBinding
 
 class FilterEightActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFiltereightBinding
+
+    lateinit var Goggle: CheckBox
+    lateinit var Amazon : CheckBox
+    lateinit var Filpkart : CheckBox
+    lateinit var Samsung: CheckBox
+    lateinit var Lenovo: CheckBox
+    lateinit var Accenture: CheckBox
+    lateinit var IBM: CheckBox
+    lateinit var Wipro: CheckBox
+    lateinit var Dell: CheckBox
+    lateinit var ClearFilter: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFiltereightBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.view.setOnClickListener {
+            val intent = Intent(this, FilterFiveActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.imageView1.setOnClickListener {
+            val intent = Intent(this, FilterTwoActivity::class.java)
+            startActivity(intent)
+        }
+
+         Goggle = findViewById(R.id.checkBox)
+         Amazon= findViewById(R.id.checkBox2)
+        Filpkart= findViewById(R.id.checkBox3)
+         Samsung= findViewById(R.id.checkBox4)
+        Lenovo= findViewById(R.id.checkBox5)
+        Accenture= findViewById(R.id.checkBox6)
+        IBM= findViewById(R.id.checkBox7)
+        Wipro= findViewById(R.id.checkBox8)
+        Dell= findViewById(R.id.checkBox9)
+        ClearFilter = findViewById(R.id.textView32)
+
+        ClearFilter.setOnClickListener{
+            if(Goggle.isChecked()) {
+                Goggle.setChecked(false)
+            }
+            if(Amazon.isChecked()) {
+                Amazon.setChecked(false)
+            }
+            if(Filpkart.isChecked()) {
+                Filpkart.setChecked(false)
+            }
+            if(Samsung.isChecked()) {
+                Samsung.setChecked(false)
+            }
+            if(Lenovo.isChecked()) {
+                Lenovo.setChecked(false)
+            }
+            if(Accenture.isChecked()) {
+                Accenture.setChecked(false)
+            }
+            if(IBM.isChecked()) {
+                IBM.setChecked(false)
+            }
+            if(Wipro.isChecked()) {
+            Wipro.setChecked(false)
+        }
+            if(Dell.isChecked()) {
+                Dell.setChecked(false)
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/workshala2/FilterFiveActivity.kt
+++ app/src/main/java/com/example/workshala2/FilterFiveActivity.kt
@@ -2,12 +2,20 @@ package com.example.workshala2
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import com.example.workshala2.databinding.ActivityFilterfiveBinding
 
 class FilterFiveActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFilterfiveBinding
+
+    lateinit var twohr : CheckBox
+    lateinit var fourhr : CheckBox
+    lateinit var sixhr : CheckBox
+    lateinit var eighthr : CheckBox
+    lateinit var ClearFilter: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFilterfiveBinding.inflate(layoutInflater)
@@ -17,6 +25,36 @@ class FilterFiveActivity : AppCompatActivity() {
             val intent = Intent(this, FilterEightActivity::class.java)
             startActivity(intent)
         }
+
+        binding.imageView1.setOnClickListener {
+            val intent = Intent(this, FilterTwoActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        twohr = findViewById(R.id.checkBox)
+        fourhr = findViewById(R.id.checkBox2)
+        sixhr = findViewById(R.id.checkBox3)
+        eighthr = findViewById(R.id.checkBox4)
+        ClearFilter = findViewById(R.id.textView32)
+
+        ClearFilter.setOnClickListener{
+            if(twohr.isChecked()) {
+                twohr.setChecked(false)
+            }
+            if(fourhr.isChecked()) {
+                fourhr.setChecked(false)
+            }
+            if(sixhr.isChecked()) {
+                sixhr.setChecked(false)
+            }
+            if(eighthr.isChecked()) {
+                eighthr.setChecked(false)
+            }
+
+        }
+
+
 
     }
 }

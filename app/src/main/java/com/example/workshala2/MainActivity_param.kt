7c3ec58bcdcc41e3ab5efc6ab4f243c8bd87e0c4
params19 <paramsrivastava5@gmail.com>
2023-11-18 23:21:55 -0800

    category page linked

--- app/src/main/java/com/example/workshala2/MainActivity_param.kt
+++ app/src/main/java/com/example/workshala2/MainActivity_param.kt
@@ -1,53 +1,70 @@
 package com.example.workshala2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity_param : AppCompatActivity() {
 
-    private var recyclerView : RecyclerView? = null
-    private var recyclerViewCategoryAdapter : RecyclerViewCategoryAdapter? = null
+    private var recyclerView: RecyclerView? = null
+    private var recyclerViewCategoryAdapter: RecyclerViewCategoryAdapter? = null
     private var movieList = mutableListOf<Category>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_param)
 
-        movieList = ArrayList()
 
-        recyclerView = findViewById<View>(R.id.rvMovieLists) as RecyclerView
-        recyclerViewCategoryAdapter = RecyclerViewCategoryAdapter(this@MainActivity_param, movieList)
-        val layoutManager : RecyclerView.LayoutManager = GridLayoutManager(this,2)
-        recyclerView!!.layoutManager = layoutManager
-        recyclerView!!.adapter = recyclerViewCategoryAdapter
+        val aboutDoc = findViewById<TextView>(R.id.next_button)
+        aboutDoc.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
 
-        prepareMovieListData()
+            movieList = ArrayList()
 
+            recyclerView = findViewById<View>(R.id.rvMovieLists) as RecyclerView
+            recyclerViewCategoryAdapter =
+                RecyclerViewCategoryAdapter(this@MainActivity_param, movieList)
+            val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 2)
+            recyclerView!!.layoutManager = layoutManager
+            recyclerView!!.adapter = recyclerViewCategoryAdapter
+
+            prepareMovieListData()
+
+
+        }
+
+        fun prepareMovieListData() {
+            var category = Category("Crypto", R.drawable.icon_crypto1)
+            movieList.add(category)
+
+            category = Category("Banking", R.drawable.icon_banking)
+            movieList.add(category)
+            category = Category("Programming", R.drawable.icon_programming)
+            movieList.add(category)
+            category = Category("Food & Restaurant", R.drawable.icon_restaurant)
+            movieList.add(category)
+            category = Category("Human Resources", R.drawable.icon_human)
+            movieList.add(category)
+            category = Category("Content Writer", R.drawable.icon_contentwriter)
+            movieList.add(category)
+            category = Category("Arts and Design", R.drawable.icon_arts)
+            movieList.add(category)
+            category = Category("Customer Services", R.drawable.icon_customer)
+            movieList.add(category)
+
+            recyclerViewCategoryAdapter!!.notifyDataSetChanged()
+        }
     }
 
-    private fun prepareMovieListData(){
-        var category = Category("Crypto", R.drawable.icon_crypto1)
-        movieList.add(category)
-
-        category = Category("Banking", R.drawable.icon_banking)
-        movieList.add(category)
-        category = Category("Programming", R.drawable.icon_programming)
-        movieList.add(category)
-        category = Category("Food & Restaurant", R.drawable.icon_restaurant)
-        movieList.add(category)
-        category = Category("Human Resources", R.drawable.icon_human)
-        movieList.add(category)
-        category = Category("Content Writer", R.drawable.icon_contentwriter)
-        movieList.add(category)
-        category = Category("Arts and Design", R.drawable.icon_arts)
-        movieList.add(category)
-        category = Category("Customer Services", R.drawable.icon_customer)
-        movieList.add(category)
-
-        recyclerViewCategoryAdapter!!.notifyDataSetChanged()
+    private fun prepareMovieListData() {
+        TODO("Not yet implemented")
     }
 }
+
 

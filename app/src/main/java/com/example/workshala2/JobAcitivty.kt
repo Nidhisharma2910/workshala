697fa761f25a155f9576087c604b56ee6f12ee27
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-12-01 23:51:08 +0530

    linking of ui/ux

--- app/src/main/java/com/example/workshala2/JobAcitivty.kt
+++ app/src/main/java/com/example/workshala2/JobAcitivty.kt
@@ -1,25 +1,31 @@
 package com.example.workshala2
 
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import com.example.workshala2.R
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
-
-
-
 class JobActivity : AppCompatActivity() {
     var tabLayout: TabLayout? = null
     var viewPager2: ViewPager2? = null
     var myViewPagerAdapter: MyViewPagerAdapter? = null
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job)
+
+        val apply = findViewById<Button>(R.id.buttonn)
+        apply.setOnClickListener{
+            val intent = Intent(this, ui_ux1::class.java)
+            startActivity(intent)
+        }
         tabLayout = findViewById<TabLayout>(R.id.tab_layout)
         viewPager2 = findViewById<ViewPager2>(R.id.view_pager)
         myViewPagerAdapter = MyViewPagerAdapter(this)
--- app/src/main/java/com/example/workshala2/ui_ux1.kt
+++ app/src/main/java/com/example/workshala2/ui_ux1.kt
@@ -1,11 +1,19 @@
 package com.example.workshala2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ui_ux1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui_ux1)
+
+        val upload = findViewById<Button>(R.id.button2)
+        upload.setOnClickListener{
+            val intent = Intent(this, ui_ux2::class.java)
+            startActivity(intent)
+        }
     }
 }
